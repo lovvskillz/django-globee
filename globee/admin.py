24cfa8add918dbf38efc2fc0a179476f53fe3e89
vadim <vadim17@hotmail.de>
2018-07-27 00:26:47 +0200

    add models to admin

--- globee/admin.py
+++ globee/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from globee.models import GlobeeIPN
+
+
+class GlobeeIPNAdmin(admin.ModelAdmin):
+    fields = ['pk', 'payment_status', 'payment_id', 'total', 'currency', 'custom_payment_id', 'created_at']
+
+
+admin.site.register(GlobeeIPN, GlobeeIPNAdmin)
+
