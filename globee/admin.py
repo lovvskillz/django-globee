79cd0fbdd0ba49326eded368a798ba0a7bc9613a
vadim <vadim17@hotmail.de>
2018-07-27 11:23:21 +0200

    add search fields

--- globee/admin.py
+++ globee/admin.py
@@ -5,6 +5,8 @@ from globee.models import GlobeeIPN
 
 class GlobeeIPNAdmin(admin.ModelAdmin):
     list_display = ('payment_id', 'payment_status', 'total', 'currency', 'custom_payment_id', 'created_at')
+    date_hierarchy = 'created_at'
+    search_fields = ('payment_id', 'custom_payment_id')
 
 
 admin.site.register(GlobeeIPN, GlobeeIPNAdmin)
