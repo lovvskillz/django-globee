48082d50e3f2ce8053355b68cd512acd7dcd1c97
vadim <vadim17@hotmail.de>
2018-07-27 11:03:51 +0200

    typo

--- globee/admin.py
+++ globee/admin.py
@@ -4,7 +4,7 @@ from globee.models import GlobeeIPN
 
 
 class GlobeeIPNAdmin(admin.ModelAdmin):
-    list_diplay = ('pk', 'payment_status', 'payment_id', 'total', 'currency', 'custom_payment_id', 'created_at')
+    list_display = ('pk', 'payment_status', 'payment_id', 'total', 'currency', 'custom_payment_id', 'created_at')
 
 
 admin.site.register(GlobeeIPN, GlobeeIPNAdmin)
